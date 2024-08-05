3030d3b52c02e2f01b3c2028ee2412b80c926f17
Om Patel <[97650590+om0611@users.noreply.github.com]>
2024-08-05 12:04:28 -0400

    Create functions that get user and computer choice
    Created a function that randomly chooses between rock, paper, and scissors
    for the computer choice. Created a function that gets user's choice.

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+const CHOICES = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+    return CHOICES[Math.floor(Math.random() * CHOICES.length)];
+}
+
+function getHumanChoice() {
+    let userInput = prompt("Enter your choice (rock, paper, scissors): ");
+    while (!CHOICES.includes(userInput)) {
+        userInput = prompt("Invalid choice.\nEnter your choice (rock, paper, scissors): ");
+    }
+    return userInput;
+
+}
+
+console.log(getComputerChoice());
+console.log(getHumanChoice());
