daee2a6671c71d8bd9ee48cd643260aca4426e5b
Om Patel <[97650590+om0611@users.noreply.github.com]>
2024-08-26 15:43:16 -0400

    Add CSS styling

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const CHOICES = ["rock", "paper", "scissors"];
 let humanScore = 0;
 let computerScore = 0;
 
-let welcomeDiv = document.querySelector('.welcome');
+let headingDiv = document.querySelector('.heading');
 let humanChoiceDiv = document.querySelector('.humanChoice');
 let computerChoiceDiv = document.querySelector('.computerChoice');
 let roundWinnerDiv = document.querySelector('.roundWinner');
@@ -10,9 +10,20 @@ let humanScoreDiv = document.querySelector('.humanScore');
 let computerScoreDiv = document.querySelector('.computerScore');
 let gameWinnerDiv = document.querySelector('.gameWinner');
 
+let buttons = document.querySelector('.buttons');
 let rockBtn = document.querySelector('#rock');
 let paperBtn = document.querySelector('#paper');
 let scissorsBtn = document.querySelector('#scissors');
+
+let playRock = function () {
+    playRound('rock', getComputerChoice());
+};
+let playPaper = function () {
+    playRound('paper', getComputerChoice());
+};
+let playScissors = function () {
+    playRound('scissors', getComputerChoice());
+};
 
 
 function getComputerChoice() {
@@ -28,18 +39,18 @@ function playRound(humanChoice, computerChoice) {
     computerChoiceDiv.textContent = `Computer: ${capitalize(computerChoice)}\n`
 
     if (humanChoice === computerChoice) {
-        roundWinnerDiv.textContent = `Tie! You and the computer both chose ${capitalize(humanChoice)}.`;
+        roundWinnerDiv.textContent = `Tie!`;
     }
     else if (
         (humanChoice === "rock" && computerChoice === "scissors") 
         || (humanChoice === "paper" && computerChoice === "rock") 
         || (humanChoice === "scissors" && computerChoice === "paper")) {
         humanScore++;
-        roundWinnerDiv.textContent = `You win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}.`;
+        roundWinnerDiv.textContent = `You win!`;
     }
     else {
         computerScore++;
-        roundWinnerDiv.textContent = `You lose! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}.`;
+        roundWinnerDiv.textContent = `You lose!`;
     }
 
     humanScoreDiv.textContent = `Your Score: ${humanScore}`;
@@ -57,24 +68,12 @@ function checkWinner() {
     else {
         gameWinnerDiv.textContent = 'The computer won the game. Better luck next time!';
     }
-    welcomeDiv.remove();
-    humanChoiceDiv.remove();
-    computerChoiceDiv.remove();
-    roundWinnerDiv.remove();
-    humanScoreDiv.remove();
-    computerScoreDiv.remove();
-    rockBtn.remove();
-    paperBtn.remove();
-    scissorsBtn.remove();
+    rockBtn.removeEventListener('click', playRock);
+    paperBtn.removeEventListener('click', playPaper);
+    scissorsBtn.removeEventListener('click', playScissors);
     return;
 }
 
-rockBtn.addEventListener('click', () => {
-    playRound('rock', getComputerChoice());
-});
-paperBtn.addEventListener('click', () => {
-    playRound('paper', getComputerChoice());
-});
-scissorsBtn.addEventListener('click', () => {
-    playRound('scissors', getComputerChoice());
-});
+rockBtn.addEventListener('click', playRock);
+paperBtn.addEventListener('click', playPaper);
+scissorsBtn.addEventListener('click', playScissors);
