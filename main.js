fd016bcd0b95f4f9ad7cb79aec24ec6c56a089e1
Om Patel <[97650590+om0611@users.noreply.github.com]>
2024-08-26 14:24:54 -0400

    Modify HTML and implement JavaScript

--- main.js
+++ main.js
@@ -1,53 +1,80 @@
-function playGame() {
-    const CHOICES = ["rock", "paper", "scissors"];
-    let humanScore = 0;
-    let computerScore = 0;
+const CHOICES = ["rock", "paper", "scissors"];
+let humanScore = 0;
+let computerScore = 0;
 
-    function getComputerChoice() {
-        return CHOICES[Math.floor(Math.random() * CHOICES.length)];
-    }
+let welcomeDiv = document.querySelector('.welcome');
+let humanChoiceDiv = document.querySelector('.humanChoice');
+let computerChoiceDiv = document.querySelector('.computerChoice');
+let roundWinnerDiv = document.querySelector('.roundWinner');
+let humanScoreDiv = document.querySelector('.humanScore');
+let computerScoreDiv = document.querySelector('.computerScore');
+let gameWinnerDiv = document.querySelector('.gameWinner');
 
-    function getHumanChoice() {
-        let userInput = prompt("Enter your choice (rock, paper, scissors): ").toLowerCase();
-        while (!CHOICES.includes(userInput)) {
-            userInput = prompt("Invalid choice.\nEnter your choice (rock, paper, scissors): ").toLowerCase();
-        }
-        return userInput;
-    }
+let rockBtn = document.querySelector('#rock');
+let paperBtn = document.querySelector('#paper');
+let scissorsBtn = document.querySelector('#scissors');
 
-    function capitalize(s) {
-        return s[0].toUpperCase() + s.slice(1);
-    }
 
-    function playRound(humanChoice, computerChoice) {
-        let roundInfo = `You: ${capitalize(humanChoice)}\nComputer: ${capitalize(computerChoice)}\n`
-
-        if (humanChoice === computerChoice) {
-            roundInfo += `Tie! You and the computer both chose ${capitalize(humanChoice)}.`;
-        }
-        else if (
-            (humanChoice === "rock" && computerChoice === "scissors") 
-            || (humanChoice === "paper" && computerChoice === "rock") 
-            || (humanChoice === "scissors" && computerChoice === "paper")) {
-            humanScore++;
-            roundInfo += `You win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}.`;
-        }
-        else {
-            computerScore++;
-            roundInfo += `You lose! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}.`;
-        }
-
-        alert(roundInfo);
-        alert(`User Score: ${humanScore}\nComputer Score: ${computerScore}`);
-    }
-
-    alert("Welcome to Rock Paper Scissors!\nFirst to win 3 rounds wins the game.")
-
-    while (humanScore < 3 && computerScore < 3) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-
-    humanScore > computerScore ? alert("Congratulations, you are the winner of this game!") : alert("The computer is the winner of this game.");
+function getComputerChoice() {
+    return CHOICES[Math.floor(Math.random() * CHOICES.length)];
 }
 
-playGame();
+function capitalize(s) {
+    return s[0].toUpperCase() + s.slice(1);
+}
+
+function playRound(humanChoice, computerChoice) {
+    humanChoiceDiv.textContent = `You: ${capitalize(humanChoice)}`
+    computerChoiceDiv.textContent = `Computer: ${capitalize(computerChoice)}\n`
+
+    if (humanChoice === computerChoice) {
+        roundWinnerDiv.textContent = `Tie! You and the computer both chose ${capitalize(humanChoice)}.`;
+    }
+    else if (
+        (humanChoice === "rock" && computerChoice === "scissors") 
+        || (humanChoice === "paper" && computerChoice === "rock") 
+        || (humanChoice === "scissors" && computerChoice === "paper")) {
+        humanScore++;
+        roundWinnerDiv.textContent = `You win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}.`;
+    }
+    else {
+        computerScore++;
+        roundWinnerDiv.textContent = `You lose! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}.`;
+    }
+
+    humanScoreDiv.textContent = `Your Score: ${humanScore}`;
+    computerScoreDiv.textContent = `Computer Score: ${computerScore}`;
+
+    checkWinner();
+}
+
+function checkWinner() {
+    if (humanScore < 5 && computerScore < 5) return; 
+
+    if (humanScore == 5) {
+        gameWinnerDiv.textContent = 'Congratulations! You won the game.';
+    }
+    else {
+        gameWinnerDiv.textContent = 'The computer won the game. Better luck next time!';
+    }
+    welcomeDiv.remove();
+    humanChoiceDiv.remove();
+    computerChoiceDiv.remove();
+    roundWinnerDiv.remove();
+    humanScoreDiv.remove();
+    computerScoreDiv.remove();
+    rockBtn.remove();
+    paperBtn.remove();
+    scissorsBtn.remove();
+    return;
+}
+
+rockBtn.addEventListener('click', () => {
+    playRound('rock', getComputerChoice());
+});
+paperBtn.addEventListener('click', () => {
+    playRound('paper', getComputerChoice());
+});
+scissorsBtn.addEventListener('click', () => {
+    playRound('scissors', getComputerChoice());
+});
